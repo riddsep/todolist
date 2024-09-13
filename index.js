223309a65e05de03227ed6430f9847108e0eee7c
riddsep <ridoseptiawan00@gmail.com>
2024-09-14 00:28:28 +0700

    fix: todos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const todos = [];
+let todos = [];
 
 function addTodo() {
   const inputTodoEl = document.getElementById("input-todo");
@@ -38,7 +38,11 @@ function showTodos() {
     );
     const componentLi = ` 
             <div class="flex items-center">
-                <input type="checkbox" class="mr-2" data-id="${todoItem.id}"/>
+                <input type="checkbox" class="mr-2" id="input-${
+                  todoItem.id
+                }" data-id="${todoItem.id}" ${
+      todoItem.done ? "checked" : ""
+    } onchange />
                     <span class="${
                       todoItem.done ? "line-through text-gray-500" : ""
                     }" >
@@ -47,12 +51,14 @@ function showTodos() {
             </div>
             <button
                 class="delete-button bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
+                onclick="deleteTodo(${todoItem.id})"
             >
                 Remove
             </button>`;
     createLi.innerHTML = componentLi;
     todoListEl.appendChild(createLi);
-    deleteTodo();
+
+    // deleteTodo();
     checkedTodo();
   });
 }
@@ -64,22 +70,36 @@ function checkedTodo() {
     checkbox.addEventListener("change", () => {
       const checkboxId = checkbox.getAttribute("data-id");
 
-      todos.forEach((todo) => {
-        if (todo.id == checkboxId) {
-          todo.done = checkbox.checked;
-        }
-      });
+      // todos.forEach((todo) => {
+      //   if (todo.id == checkboxId) {
+      //     todo.done = checkbox.checked;
+      //   } else {
+      //     todo.done = false
+      //   }
+      // });
+      const tempTodo = todos.map((todo) => ({
+        ...todo,
+        done: todo.id == checkboxId ? !todo.done : todo.done,
+      }));
+      todos = tempTodo;
+      console.log(todos);
       showTodos();
     });
   });
 }
 
-function deleteTodo() {
-  const deleteButton = document.querySelectorAll(".delete-button");
-  deleteButton.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      todos.splice(index, 1);
-      showTodos();
-    });
-  });
+// function deleteTodo() {
+//   const deleteButton = document.querySelectorAll(".delete-button");
+//   deleteButton.forEach((button, index) => {
+//     button.addEventListener("click", () => {
+//       todos.splice(index, 1);
+//       showTodos();
+//     });
+//   });
+// }
+
+function deleteTodo(id) {
+  const deletedArray = todos.filter((todo) => todo.id !== id);
+  todos = deletedArray;
+  showTodos();
 }
